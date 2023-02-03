173e37a92bf9174d5ab33f20f89934f1821a5dc8
jssklynn <jessikalynnadams@gmail.com>
2023-02-03 09:57:48 -0700

    pig latin translator

---
+++ Python Pig Latin Converter/app.py
@@ -0,0 +1,28 @@
+def pigLatin():
+    ay = "ay"
+    yay = "yay"
+    newSentence = []
+    original = input("""
+Please enter an English word or phrase: """)
+
+    if all(character.isalpha() or character.isspace() for character in original):
+        original = original.split()
+        for word in original:
+            firstLetter = word[0]
+            if firstLetter in ["a", "e", "i", "o", "u"]:
+                newWord = word + yay
+                newSentence.append(newWord)
+            else:
+                newWord = word[1:] + word[0] + ay
+                newSentence.append(newWord)
+        newSentence = " ".join(newSentence)
+        print(newSentence)
+    else:
+        invalidMenu = input("""
+Invalid entry. Enter 1 to quit or 2 to continue.""")
+        if invalidMenu == "1":
+            quit
+        elif invalidMenu == "2":
+            pigLatin()
+
+pigLatin()
